ad85d40221be4d2784d33999fd957dd89d36796d
Shubahm <shubham.20205157@mnnit.ac.in>
2023-01-30 02:55:44 +0530

    SignUp Page

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDb = require('./config/db');
+var cors = require('cors');
+
+const app = express();
+connectDb();
+
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+app.get('/', (req, res) => res.send('API running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
